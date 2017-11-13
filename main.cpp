89979bffad12540ab911d7caf1cf12cee25b2e5f
Piotr <pik9603@gmail.com>
2017-11-13 12:38:44 +0100

    Added gitignore file.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "boxes_aficionado/Box.hpp"
+#include "boxes_aficionado/algorithms/BrutForceAlgorithm.hpp"
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
+using namespace boxes_aficionado;
+using namespace boxes_aficionado::algorithms;
+
+int main(int argc, char** argv) {
+
+
+	std::vector<Box> boxes = {{1,2,3}, {4,2,3}, };
+
+	std::cout << "Min : "  << BrutForceAlgorithm().compute(boxes) << std::endl;
+
 	return 0;
 }

a120b277ae56b9924c9173e8af3d1ab939cfab2a
Piotr <pik9603@gmail.com>
2018-01-17 21:59:56 +0100

    script fixes
    Signed-off-by: Piotr <pik9603@gmail.com>

--- sources/scripts/correctness_testing.sh
+++ sources/scripts/correctness_testing.sh
@@ -3,7 +3,7 @@
 for i in tests/correctness_tests/*.in ; do
     test=${i%.*}
     echo -n "Running test ${test} : "
-    ../../cmake-build-debug/AAL_Boxes -m 2 -i "${test}.in" -o "out.out"
+    ../../cmake-build-debug/AAL_Boxes -m 2 -i "${test}.in" -o "out.out" -a 1
     awk '(NR==FNR && FNR==2) {result[$0]++; nextfile} ; {if(!($0 in result) && FNR==2) exit 1 }' "${test}.out" out.out 
     if [ "${?}" -ne "0" ]; then
         echo "failed"
